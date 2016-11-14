51f49f7c17d2d816435242e6d2a2d4e690c5dff2
mtarazona19 <mtarazona@gmail.com>
2016-11-13 20:11:26 -0500

    Se subieron los archivos del Graficador
    Corresponde a todos los archivos fuentes del programa Graficador en la versión 1.0

---
+++ Graficador/Graficador/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(25, 33)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Graficar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(137, 33)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(586, 279)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Gráfico"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"y=x", "y=x^2", "y=x^3", "y=sen(x)", "y=cos(x)", "y=tan(x)"})
+        Me.ListBox1.Location = New System.Drawing.Point(25, 80)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(75, 108)
+        Me.ListBox1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(768, 361)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ Graficador/Graficador/Form1.vb
@@ -0,0 +1,157 @@
+﻿Public Class Form1
+    Public Sub New()
+        InitializeComponent()
+        Ejes()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim puntos As Integer
+        Dim yreal, y, ymax, ymin, xrad, factory, xreal, x, xmax, xmin, factorx, salto As Double
+        Dim tlbx As String
+
+        tlbx = ListBox1.GetItemText(ListBox1.SelectedItem)
+        If tlbx = "" Then
+            MsgBox("Debe seleccionar la función que desea graficar")
+            Return
+        End If
+
+        ymax = 0
+        ymin = 0
+        factory = 1
+        xmax = 0
+        xmin = 0
+        factorx = 1
+        puntos = 0
+        salto = 1
+
+        Ejes()
+
+        'Limites del area a graficar
+        ' -200 <= xreal <= 200
+        ' -100 <= yreal <= 100
+        'correccion al graficar
+        ' x = 300 + xreal
+        ' y = 150 - yreal
+
+        For xreal = -200 To 200
+
+            xrad = xreal * 3.1416 / 180
+  
+            Select Case tlbx
+                Case "y=x"
+                    yreal = xreal
+                Case "y=x^2"
+                    yreal = xreal * xreal
+                Case "y=x^3"
+                    yreal = xreal * xreal * xreal
+                Case "y=sen(x)"
+                    yreal = Math.Sin(xrad)
+                Case "y=cos(x)"
+                    yreal = Math.Cos(xrad)
+                Case "y=tan(x)"
+                    yreal = Math.Tan(xrad)
+            End Select
+
+            If (yreal >= -100 And yreal <= 100) Then
+                'correccion y
+                If (yreal > ymax) Then
+                    ymax = yreal
+                End If
+                If (yreal < ymin) Then
+                    ymin = yreal
+                End If
+                If (ymax < 0.1 * 100 And ymin > 0.1 * -100) Then
+                    factory = 80 / ymax
+                End If
+                'correccion x
+                If (xreal > xmax) Then
+                    xmax = xreal
+                End If
+                If (xreal < xmin) Then
+                    xmin = xreal
+                End If
+                If (xmax < 0.1 * 200 And xmin > 0.1 * -200) Then
+                    factorx = 80 / xmax
+                End If
+                puntos = puntos + 1
+            End If
+
+        Next xreal
+
+        If puntos < 200 Then
+            salto = (xmax - xmin) / 200
+        End If
+
+        For xreal = xmin To xmax Step salto
+
+            xrad = xreal * 3.1416 / 180
+
+            Select Case tlbx
+                Case "y=x"
+                    yreal = xreal
+                Case "y=x^2"
+                    yreal = xreal * xreal
+                Case "y=x^3"
+                    yreal = xreal * xreal * xreal
+                Case "y=sen(x)"
+                    yreal = Math.Sin(xrad)
+                Case "y=cos(x)"
+                    yreal = Math.Cos(xrad)
+                Case "y=tan(x)"
+                    yreal = Math.Tan(xrad)
+            End Select
+
+            If (yreal >= -100 And yreal <= 100) Then
+                'correccion
+                y = 150 - yreal * factory
+                x = 300 + xreal * factorx
+                Escribe(x, y, 1)
+            End If
+        Next xreal
+        'MsgBox(factorx)
+    End Sub
+
+    Private Function Escribe(ByVal x As Integer, y As Integer, w As Integer) As Integer
+        Dim lb As New Label
+
+        lb.Text = ""
+        lb.MaximumSize = New Size(w, w)
+        lb.Location = New Point(x, y)
+        lb.BackColor = Color.Black
+        GroupBox1.Controls.Add(lb)
+
+    End Function
+    Private Function EscribeTxt(ByVal x As Integer, y As Integer, t As String) As Integer
+        Dim lb As New Label
+
+        lb.Text = t
+        lb.AutoSize = True
+        lb.Location = New Point(x, y)
+        GroupBox1.Controls.Add(lb)
+
+    End Function
+    Private Function Ejes()
+        Dim var As Integer
+
+        GroupBox1.Controls.Clear()
+        'dibujar ejes x,y
+        For var = 100 To 500
+            'eje x
+            Escribe(var, 150, 2)
+        Next var
+        For var = 50 To 250
+            'eje y
+            Escribe(300, var, 2)
+        Next var
+
+        EscribeTxt(510, 145, "X")
+        EscribeTxt(295, 30, "Y")
+
+    End Function
+
+
+End Class
+
+
+
+
